8d0ba2f5e7d2566608eee0dcc87f0e403134376b
Jeroen van der Heijden <jeroen@transceptor.technology>
2018-07-05 16:28:36 +0200

    Remove steps in doc

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@
 
 Upload to PyPI, Thx to: http://peterdowns.com/posts/first-time-with-pypi.html
 
-python3 setup.py register -r pypitest
 python3 setup.py sdist upload -r pypitest
 
-python3 setup.py register -r pypi
 python3 setup.py sdist upload -r pypi
 """
 from distutils.core import setup
